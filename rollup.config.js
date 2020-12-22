2ad91f19b31dde09fb3a4b8bb0d16740128defc3
Nick Rakochy <nick.rakochy@nuvolo.com>
2020-12-22 16:34:05 -0600

    Change output name

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,13 @@
-import commonjs from "@rollup/plugin-commonjs";
-import { terser } from "rollup-plugin-terser";
-import typescript from "rollup-plugin-typescript2";
+import commonjs from '@rollup/plugin-commonjs';
+import { terser } from 'rollup-plugin-terser';
+import typescript from 'rollup-plugin-typescript2';
 
 const config = {
-  input: "src/index.ts",
+  input: 'src/index.ts',
   output: {
-    file: "dist/bundle.js",
-    format: "umd",
-    name: "SNSocket",
+    file: 'dist/sn-socket.min.js',
+    format: 'umd',
+    name: 'SNSocket',
   },
   plugins: [typescript(), commonjs(), terser()],
 };
